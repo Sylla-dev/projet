6cef4d46e4930ce90a1acdd23aa48c0522c85835
Sylla-dev <kerfala744@gmail.com>
2025-07-26 22:48:24 +0100

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // 1️⃣ Configuration d'une instance Axios avec l'URL de base
 export const api = axios.create({
-  baseURL: 'http://localhost:4000', // Adapter selon la config serveur
+  baseURL: 'https://gestions-backend.onrender.com', // Adapter selon la config serveur
   headers: {
     'Content-Type': 'application/json'
   },
